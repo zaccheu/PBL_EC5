09c8f8a713332b4787402d6cb12825999f697553
zaccheu <gu.zaccheu8@gmail.com>
2025-05-25 11:10:54 -0300

    Ajuste da classe DAO de Estufa

--- PBL_EC5/DAO/EstufaDAO.cs
+++ PBL_EC5/DAO/EstufaDAO.cs
@@ -8,49 +8,47 @@ namespace PBL_EC5.Models.DAO
     {
         protected override SqlParameter[] CriaParametros(EstufaViewModel estufa)
         {
-            SqlParameter[] parametros = new SqlParameter[6];
-            parametros[0] = new SqlParameter("idEstufa", estufa.IdEstufa);
-            parametros[1] = new SqlParameter("nome", estufa.Nome);
-            parametros[2] = new SqlParameter("localizacao", estufa.Localizacao);
-            parametros[3] = new SqlParameter("descricao", estufa.Descricao);
-            parametros[4] = new SqlParameter("areaEstufaM2", estufa.AreaEstufaM2);
-            parametros[5] = new SqlParameter("alturaM", estufa.AlturaM);
+            SqlParameter[] parametros = new SqlParameter[7];
+            parametros[0] = new SqlParameter("Id", estufa.Id); // Chave primária
+            parametros[1] = new SqlParameter("Id_Cliente", estufa.Id_Cliente); // Relacionamento com Cliente
+            parametros[2] = new SqlParameter("Id_Estado", estufa.Id_Estado); // Relacionamento com Estado
+            parametros[3] = new SqlParameter("Numero_Serie", estufa.Numero_Serie); // Número de série
+            parametros[4] = new SqlParameter("Marca", estufa.Marca); // Marca
+            parametros[5] = new SqlParameter("Potencia", estufa.Potencia); // Potência
+            parametros[6] = new SqlParameter("Tensao", estufa.Tensao); // Tensão elétrica
 
             return parametros;
         }
 
         protected override EstufaViewModel MontaModel(DataRow registro)
         {
-            EstufaViewModel a = new EstufaViewModel();
-            a.Id = Convert.ToInt32(registro["id"]);
-            a.IdEstufa = registro.Table.Columns.Contains("idEstufa") && registro["idEstufa"] != DBNull.Value
-                ? Convert.ToInt32(registro["idEstufa"])
+            EstufaViewModel estufa = new EstufaViewModel();
+            estufa.Id = Convert.ToInt32(registro["Id"]);
+            estufa.Id_Cliente = registro.Table.Columns.Contains("Id_Cliente") && registro["Id_Cliente"] != DBNull.Value
+                ? Convert.ToInt32(registro["Id_Cliente"])
                 : 0;
-            a.Nome = registro.Table.Columns.Contains("nome") && registro["nome"] != DBNull.Value
-                ? registro["nome"].ToString()
+            estufa.Id_Estado = registro.Table.Columns.Contains("Id_Estado") && registro["Id_Estado"] != DBNull.Value
+                ? Convert.ToInt32(registro["Id_Estado"])
+                : 0;
+            estufa.Numero_Serie = registro.Table.Columns.Contains("Numero_Serie") && registro["Numero_Serie"] != DBNull.Value
+                ? registro["Numero_Serie"].ToString()
                 : string.Empty;
-            a.Localizacao = registro.Table.Columns.Contains("localizacao") && registro["localizacao"] != DBNull.Value
-                ? registro["localizacao"].ToString()
+            estufa.Marca = registro.Table.Columns.Contains("Marca") && registro["Marca"] != DBNull.Value
+                ? registro["Marca"].ToString()
                 : string.Empty;
-            a.Descricao = registro.Table.Columns.Contains("descricao") && registro["descricao"] != DBNull.Value
-                ? registro["descricao"].ToString()
-                : string.Empty;
-            a.AreaEstufaM2 = registro.Table.Columns.Contains("areaEstufaM2") && registro["areaEstufaM2"] != DBNull.Value
-                ? Convert.ToSingle(registro["areaEstufaM2"])
-                : (float?)null;
-            a.AlturaM = registro.Table.Columns.Contains("alturaM") && registro["alturaM"] != DBNull.Value
-                ? Convert.ToSingle(registro["alturaM"])
-                : (float?)null;
-            a.DataAtualizacao = registro.Table.Columns.Contains("dataAtualizacao") && registro["dataAtualizacao"] != DBNull.Value
-                ? Convert.ToDateTime(registro["dataAtualizacao"])
-                : (DateTime?)null;
+            estufa.Potencia = registro.Table.Columns.Contains("Potencia") && registro["Potencia"] != DBNull.Value
+                ? Convert.ToDouble(registro["Potencia"])
+                : 0.0;
+            estufa.Tensao = registro.Table.Columns.Contains("Tensao") && registro["Tensao"] != DBNull.Value
+                ? Convert.ToInt32(registro["Tensao"])
+                : 0;
 
-            return a;
+            return estufa;
         }
 
         protected override void SetTabela()
         {
-            Tabela = "dbo.Estufas";
+            Tabela = "dbo.Estufa";
         }
     }
 }
