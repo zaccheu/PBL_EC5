a68cdf959fd7b054cc461e74892e117c49d965f8
zaccheu <gu.zaccheu8@gmail.com>
2025-06-01 23:34:59 -0300

    DadosEstufaViewModel

---
+++ PBL_EC5/Models/DadosEstufaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PBL_EC5.Models
+{
+    public class DadosEstufaViewModel : PadraoViewModel
+    {
+        public int Id_Estufa { get; set; }
+        public double Temperatura { get; set; }
+        public DateTime Data { get; set; }
+        public double? Tensao { get; set; }
+    }
+}
